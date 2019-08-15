af8f936afe36e0af67f57c448a1e62d3def3b506
rogermejia <Roger.Alonso7412@gmail.com>
2019-08-15 09:46:04 -0600

    Calculadora con metodos Static

---
+++ src/CalculadoraDigital.java
@@ -0,0 +1,30 @@
+
+public class CalculadoraDigital extends Calculadora{
+
+	@Override
+	public static void suma(Double i, Double j) {
+		System.out.println(i + j);
+		
+	}
+
+	@Override
+	public static void resta(Double i, Double j) {
+		System.out.println(i - j);
+		
+	}
+
+	@Override
+	public static void multiplicacion(Double i, Double j) {
+		System.out.println(i * j);
+		
+	}
+
+	@Override
+	public static void division(Double i, Double j) {
+		System.out.println(i / j);
+		
+	}
+
+	
+
+}
---
+++ src/Inicio.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Inicio {
+	//MODIFICACION PARA OPERAR CON METODOS STATIC
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		Double valor1 = 0.0;
+		Double operacion = 0.0;
+		Double valor2 = 0.0;
+		boolean check = true;
+		boolean check2 = true;
+		
+		System.out.println("Calculadora con métodos Static");
+		System.out.println("Ingrese un número");
+		
+		do {
+			s = new Scanner(System.in);
+			try {
+				valor1 = s.nextDouble();
+				check = valor1.isNaN();
+			} catch (Exception e) {
+				System.out.println("Ese no es un numero");
+				check = true;
+			}
+			
+		}while(check != false);
+		
+		
+		System.out.println("Digite: 1 para Suma, 2 para Resta, 3 para Multiplicacion, 4 para Division");
+		
+			
+		do {
+			
+			s = new Scanner(System.in);
+	
+			try {
+				operacion = s.nextDouble();
+				check = operacion.isNaN();
+				check2 = false;
+				
+			} catch (Exception e) {
+				System.out.println("Esa no es una opcion de operador");
+				check = true;
+			}
+			
+			if(operacion == 1 || operacion == 2 || operacion == 3 || operacion == 4 || check2 == true) {
+				check = false;
+			}else {
+				check = true;
+				System.out.println("Esa no es una opcion de operador");
+			}
+						
+		}while(check != false);
+		
+		
+		
+		
+		
+
+		
+		
+
+		System.out.println("Ingrese otro número");
+		
+		do {
+			
+			s = new Scanner(System.in);
+			try {
+				valor2 = s.nextDouble();
+				check = valor2.isNaN();
+			} catch (Exception e) {
+				System.out.println("Ese no es un numero");
+				check = true;
+			}
+			
+		}while(check != false);
+			
+
+			
+	
+			
+		//CalculadoraDigital d = new CalculadoraDigital();
+		if (operacion==1) {
+			CalculadoraDigital.suma(valor1, valor2);
+		} else if (operacion==2) {
+			CalculadoraDigital.resta(valor1, valor2);
+		} else if (operacion==3) {
+			CalculadoraDigital.multiplicacion(valor1, valor2);
+		} else if (operacion==4) {
+			CalculadoraDigital.division(valor1, valor2);
+		}
+	}
+
+		
+	}
+
